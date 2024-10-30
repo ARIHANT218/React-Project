a2c56a14a35930d4d5c34f9ff8cf06fe6b76176e
ARIHANT <157610621+ARIHANT218@users.noreply.github.com>
2024-10-30 10:01:53 +0530

    react router

--- 05website/src/main.jsx
+++ 05website/src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './componenets/Home/Home.jsx'
 import About from './componenets/About/About.jsx'
 import Contact from './componenets/Contact/Contact.jsx'
-// import User from './componenets/User/User.jsx'
-// import Github, { githubInfoLoader } from './componenets/Github/Github.jsx'
+import User from './componenets/User/User.jsx'
+import Github, { githubInfoLoader } from './componenets/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -37,12 +36,12 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
-      {/* <Route path='user/:userid' element={<User />} />
+      <Route path='user/:userid' element={<User />} />
       <Route 
       loader={githubInfoLoader}
       path='github' 
       element={<Github />}
-       /> */}
+       />
     </Route>
   )
 )
